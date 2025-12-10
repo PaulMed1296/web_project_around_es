8fbedde70099e711eb57680ffc9420954a60d33f
Urielini <paul.med1296@gmail.com>
2025-12-09 16:39:22 -0800

    New update to the whole page

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Tripleten web_project_around_es
+
+Este es mi proyecto en el cual comienzo a manipular el DOM de una pagina web, el siguiente proyecto consiste en un apagina donde puedes editar nu nombre de perfil y descripcion en una ventana emergente, para una red social.
+
+Principalmente se utilizó JS como lenguaje ya que el CSS y el HTML contenian su estructura predefinida.
+
+Autor: Paúl Medina
--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,68 @@
-let  initialCards = [
-    {name: "Valle de Yosemite", link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"},
-    {name: "Lago Louise", link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"},
-    {name: "Montañas Calvas", link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"},
-    {name: "Latemar", link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"},
-    {name: "Parque Nacional de la Vanoise", link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"},
-    {name: "Lago di Braies", link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"},
-]
+let initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
 
-initialCards.forEach(card => {
-    console.log(card.name);
-});
+const profileInfo = document.querySelector(".profile__info");
+const editModal = document.querySelector("#edit-popup");
+const openModal = profileInfo.querySelector(".profile__edit-button");
+const closeModal = editModal.querySelector(".popup__close");
+const profileTitle = profileInfo.querySelector(".profile__title");
+const profileDescription = profileInfo.querySelector(".profile__description");
+let inputName = document.querySelector(".popup__input_type_name");
+let inputDescription = document.querySelector(".popup__input_type_description");
+let formElement = editModal.querySelector("#edit-profile-form");
+
+function fillProfileForm() {
+  inputName.value = profileTitle.textContent;
+  inputDescription.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal() {
+  editModal.classList.add("popup_is-opened");
+  fillProfileForm();
+}
+
+function handleCloseEditModal() {
+  editModal.classList.remove("popup_is-opened");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let newName = inputName.value;
+  let newDescription = inputDescription.value;
+
+  profileTitle.textContent = newName;
+  profileDescription.textContent = newDescription;
+
+  handleCloseEditModal();
+}
+
+openModal.addEventListener("click", handleOpenEditModal);
+
+closeModal.addEventListener("click", handleCloseEditModal);
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
